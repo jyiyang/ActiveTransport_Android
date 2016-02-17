d54aa3bd376a1e6c27174ab379beca6ddde45585
Weiyun Ma <wma@g.hmc.edu>
2016-02-17 15:57:38 -0800

    listview almost done; add buttons at the bottom

--- ActiveTransportation(Android)/app/src/main/java/activetransportation/myapplication/ChecklistActivity.java
+++ ActiveTransportation(Android)/app/src/main/java/activetransportation/myapplication/ChecklistActivity.java
@@ -1,13 +1,10 @@
 package activetransportation.myapplication;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -32,9 +29,10 @@ public class ChecklistActivity extends AppCompatActivity {
         // this-The current activity context.
         // Second param is the resource Id for list layout row item
         // Third param is input array
-        arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, studentArray);
+        arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_multiple_choice, studentArray);
         studentListView.setAdapter(arrayAdapter);
 
+        /*
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -42,7 +40,7 @@ public class ChecklistActivity extends AppCompatActivity {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });
+        });*/
     }
 
 
