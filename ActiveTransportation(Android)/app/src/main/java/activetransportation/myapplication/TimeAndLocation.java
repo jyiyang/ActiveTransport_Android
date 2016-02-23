d5e86869241ef957166c1ac9ce0bedd4744dc4c3
Jack Yang <supermualbert@gmail.com>
2016-02-23 09:00:34 +0800

    fill in missing lines of codes

--- ActiveTransportation(Android)/app/src/main/java/activetransportation/myapplication/TimeAndLocation.java
+++ ActiveTransportation(Android)/app/src/main/java/activetransportation/myapplication/TimeAndLocation.java
@@ -1,5 +1,6 @@
 package activetransportation.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,7 +9,21 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 public class TimeAndLocation extends AppCompatActivity {
+    /* Switch activities when click on tabs */
+    public void switchChecklist(View view) {
+        Intent intent = new Intent(this, ChecklistActivity.class);
+        startActivity(intent);
+    }
 
+    public void switchTimeAndLoc(View view) {
+        Intent intent = new Intent(this, TimeAndLocation.class);
+        startActivity(intent);
+    }
+
+    public void switchNotify(View view) {
+        Intent intent = new Intent(this, Notify.class);
+        startActivity(intent);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
