0692191b4b9a727ad0ad4a06151b2e568f0a36f5
Weiyun Ma <wma@g.hmc.edu>
2016-03-09 02:17:53 -0800

    realtime checkbox update done

--- ActiveTransportation(Android)/app/src/main/java/activetransportation/myapplication/CustomListAdapter.java
+++ ActiveTransportation(Android)/app/src/main/java/activetransportation/myapplication/CustomListAdapter.java
@@ -12,13 +12,20 @@ import android.widget.CompoundButton;
 import android.widget.ListAdapter;
 import android.widget.TextView;
 
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
+
 import java.util.ArrayList;
+import java.util.Map;
 
 
 public class CustomListAdapter extends BaseAdapter implements ListAdapter {
     public final static String CONTACT_INFO = "ActiveTransport.CONTACT_INFO";
     private ArrayList<Student> list = new ArrayList<Student>();
     private Context context;
+    private static final String FIREBASE_URL = "https://active-transportation.firebaseIO.com";
 
     public CustomListAdapter(ArrayList<Student> list, Context context) {
         this.list = list;
@@ -50,19 +57,48 @@ public class CustomListAdapter extends BaseAdapter implements ListAdapter {
             view = inflater.inflate(R.layout.list_row_layout, null);
         }
 
-        Student student = list.get(position);
+        final Student student = list.get(position);
 
         //Handle TextView and display string from your list
         TextView studentName = (TextView)view.findViewById(R.id.student_name);
         studentName.setText(student.getName());
 
         //Handle checkboxes and add setOnCheckedChangeListeners
-        CheckBox checkBox = (CheckBox)view.findViewById(R.id.checkbox);
+        final CheckBox checkBox = (CheckBox)view.findViewById(R.id.checkbox);
         checkBox.setChecked(false);
+
+        Firebase ref = new Firebase(FIREBASE_URL);
+        Firebase studentsRef = ref.child("students");
+        //Query queryRef = studentsRef.orderByKey().equalTo(student.getID());
+
+        // Attach an listener to read the data at our posts reference
+        studentsRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot snapshot) {
+
+                for (DataSnapshot postSnapshot : snapshot.getChildren()) {
+                    String key = postSnapshot.getKey();
+                    if (key == student.getID()) {
+                        Map<String, Object> stuMap = (Map<String, Object>) postSnapshot.getValue();
+                        checkBox.setChecked((Boolean) stuMap.get("isArrived"));
+                        break;
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+                System.out.println("The read failed: " + firebaseError.getMessage());
+            }
+        });
 
         checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 // update your model (or other business logic) based on isChecked
+                Firebase ref = new Firebase(FIREBASE_URL);
+                Firebase studentsRef = ref.child("students");
+                studentsRef.child(student.getID()).child("isArrived").setValue(isChecked);
+                student.setIsArrived(isChecked);
             }
         });
 
@@ -73,7 +109,6 @@ public class CustomListAdapter extends BaseAdapter implements ListAdapter {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(v.getContext(), ContactInfoActivity.class);
-                Student student = list.get(position);
                 String name = student.getName();  // position is index in the list
                 intent.putExtra(CONTACT_INFO, name);
                 v.getContext().startActivity(intent);
