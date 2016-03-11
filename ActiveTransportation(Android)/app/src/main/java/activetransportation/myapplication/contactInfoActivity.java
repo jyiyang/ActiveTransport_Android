8390377be042045acd196347323509d75152aa24
Jack Yang <supermualbert@gmail.com>
2016-03-11 16:25:18 +0800

    fix click return button bug

--- ActiveTransportation(Android)/app/src/main/java/activetransportation/myapplication/contactInfoActivity.java
+++ ActiveTransportation(Android)/app/src/main/java/activetransportation/myapplication/contactInfoActivity.java
@@ -2,6 +2,7 @@ package activetransportation.myapplication;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -9,9 +10,16 @@ import android.widget.TextView;
 
 public class ContactInfoActivity extends AppCompatActivity {
     public void switchChecklist(View view) {
-        Intent intent = new Intent(this, ChecklistActivity.class);
-        startActivity(intent);
+        finish();
     }
+
+//    @Override
+//    public void onBackPressed() {
+//        System.out.println("call override method");
+//        Intent intent = new Intent(this, ChecklistActivity.class);
+//        startActivity(intent);
+//    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
