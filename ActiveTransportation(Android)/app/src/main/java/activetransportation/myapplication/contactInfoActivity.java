c26397180cf5c11b9759b582ee8ccea39b290d9e
Weiyun Ma <wma@g.hmc.edu>
2016-03-11 01:03:44 -0800

    minor change in filename

--- ActiveTransportation(Android)/app/src/main/java/activetransportation/myapplication/contactInfoActivity.java
+++ ActiveTransportation(Android)/app/src/main/java/activetransportation/myapplication/contactInfoActivity.java
@@ -2,7 +2,6 @@ package activetransportation.myapplication;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -44,7 +43,7 @@ public class ContactInfoActivity extends AppCompatActivity {
         parentView.setText("Foo Bar");
 
         TextView phoneView = (TextView) findViewById(R.id.phone_num);
-        phoneView.setText("(909)424-424");
+        phoneView.setText("(909)424-4242");
 
 //        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
 //        fab.setOnClickListener(new View.OnClickListener() {
