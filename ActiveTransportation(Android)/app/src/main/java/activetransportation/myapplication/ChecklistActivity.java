20e8e650b6d85b9c4110b79e3d54ee82122db20b
Weiyun Ma <wma@g.hmc.edu>
2016-04-19 23:46:21 -0700

    clean up checklist activity

--- ActiveTransportation(Android)/app/src/main/java/activetransportation/myapplication/ChecklistActivity.java
+++ ActiveTransportation(Android)/app/src/main/java/activetransportation/myapplication/ChecklistActivity.java
@@ -269,7 +269,7 @@ public class ChecklistActivity extends AppCompatActivity {
          * as you specify a parent activity in AndroidManifest.xml.
          */
         int id = item.getItemId();
-        
+
         if (id == R.id.action_settings) {
             Intent intent = new Intent(this, SettingsActivity.class);
             intent.putExtra(OLDPASSWORD, password);
